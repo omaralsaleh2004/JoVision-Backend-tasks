c9f3cd6ea4aa406e61f9b441a2a7039cac4adc9c
Your Name <you@example.com>
2025-07-08 12:07:23 +0300

    Completed Task 45

---
+++ FirstApplication/Controllers/BirthDateController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BirthDateController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAge(
+            [FromQuery] string? name,
+            [FromQuery] int? years,
+            [FromQuery] int? months,
+            [FromQuery] int? days)
+        {
+            name = string.IsNullOrWhiteSpace(name) ? "Anonymous" : name;
+
+            if (years == null || months == null || days == null)
+            {
+                return Ok($"Hello {name}, can’t calculate your age without knowing your birthdate");
+            }
+
+
+            var birthDate = DateTime.Today.AddYears(-years.Value).AddMonths(-months.Value).AddDays(-days.Value);
+            var age = CalculateAge(birthDate);
+
+            return Ok ($"Hello {name}  Your Age is : {age} ");
+        }
+
+        private int CalculateAge(DateTime birthDate)
+        {
+            var today = DateTime.Today;
+            int age = today.Year - birthDate.Year;
+            return age;
+        }
+    }
+}
--- FirstApplication/obj/Debug/net8.0/FirstApplication.AssemblyInfo.cs
+++ FirstApplication/obj/Debug/net8.0/FirstApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2726161c2f30bf6b82e2bc35065000785e610e98")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
